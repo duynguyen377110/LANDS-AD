4c266fb7bea85f43f5a8048d05e1c7e656a38478
duy366110 <duy366110@gmail.com>
2024-03-28 16:08:42 +0700

    #Commit render template new product.

--- src/app/commons/common-select/common-select.component.ts
+++ src/app/commons/common-select/common-select.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-common-select',
   templateUrl: './common-select.component.html',
   styleUrls: ['./common-select.component.scss']
 })
-export class CommonSelectComponent {
+export class CommonSelectComponent implements OnInit {
+
+  @Input('data') data: Array<any> = [];
+  @Input('formControl') formControl: FormControl = new FormControl('', []);
+  @Input('label') label: string = '';
+  @Input('id') id: string = '';
+  @Input('submit') submit: boolean = false;
+  error: any = null;
+
+  constructor() { }
+
+  ngOnChanges(): void {
+    if(this.submit) {
+      this.renderMessage()
+    }
+  }
+
+  ngOnInit(): void { }
+
+  renderMessage(): void {
+    let errors = this.formControl.errors;
+    if(errors) {
+      let key = Object.keys(errors)[0];
+
+      this.error = {
+        error: errors[key],
+        message: errors[`${key}Message`]
+      };
+
+    } else {
+      this.error = null;
+    }
+  }
+
+  onBlurHandler(): void {
+    this.renderMessage()
+  }
 
 }
--- src/app/dashboard/dashboard-product/dashboard-product-new/dashboard-product-new.component.ts
+++ src/app/dashboard/dashboard-product/dashboard-product-new/dashboard-product-new.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ValidationsService } from 'src/app/services/validations/validations.service';
 
 @Component({
   selector: 'app-dashboard-product-new',
   templateUrl: './dashboard-product-new.component.html',
   styleUrls: ['./dashboard-product-new.component.scss']
 })
-export class DashboardProductNewComponent {
+export class DashboardProductNewComponent implements OnInit, OnDestroy {
+  formProduct: FormGroup = new FormGroup({});
+  address: FormControl = new FormControl('', [this.serviceValidation.require()]);
+  contact: FormControl = new FormControl('', [this.serviceValidation.require()]);
+  landArea: FormControl = new FormControl('', [this.serviceValidation.require()]);
+  price: FormControl = new FormControl('', [this.serviceValidation.require()]);
+  productOwner: FormControl = new FormControl('', [this.serviceValidation.require()]);
+  photos: FormControl = new FormControl('', []);
+  categories: FormControl = new FormControl('', [this.serviceValidation.require()]);
+
+  submit: boolean = false;
+  titleButton: string = 'Tạo sản phẩm';
+
+
+  constructor(
+    public fb: FormBuilder,
+    public serviceValidation: ValidationsService
+  ) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.formProduct = this.fb.group({
+      productOwner: this.productOwner,
+      address: this.address,
+      contact: this.contact,
+      landArea: this.landArea,
+      price: this.price,
+      photos: this.photos,
+      categories: this.categories
+    })
+  }
+
+  onSubmitHandler(event: any): void {
+    event.preventDefault();
+
+    this.submit = true;
+    if(this.formProduct.status !== "INVALID") {
+      this.submit = false;
+
+      let formData = new FormData();
+      console.log(this.formProduct);
+
+      // formData.append('title', this.formProduct.value.productOwner);
+      // formCategoryData.append('description', this.formCategory.value.description);
+
+
+      // let inputPhotos: any = this.formCategory.controls['photos'];
+
+      // if(inputPhotos.value.length) {
+      //     for(let file of inputPhotos.value) {
+      //       formCategoryData.append('photos', file);
+      //     }
+      // }
+
+      // let res = await this.httpSendFile.post(this.url, formCategoryData);
+      // let { status } = res;
+
+      // if(status) {
+      //   this.router.navigate(['..'], {relativeTo: this.route});
+      // }
+      
+    }
+  }
+
+  ngOnDestroy(): void {
+    
+  }
 
 }
